9921de9089993b182bd3f17e6dd76e63b7c0a6ec
Amol Mahor <150514111+amolmahor50@users.noreply.github.com>
2024-11-14 09:00:26 +0000

    add year, month and day

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 
-// declaration and initilization variable
-let submit = document.getElementById("submit");
-let output = document.getElementById("output");
 
-submit.addEventListener("click", ()=>{
-    // create date objetct inpute value 
-    let date1 = new Date(document.getElementById("date-1").value);
-    let date2 = new Date(document.getElementById("date-2").value);
-    let span = document.querySelectorAll("span");
+$(document).ready(function() {
+    $('#submit').click(function() {
 
-    console.log(date1);
-    
-    //check if the input dates are valid if valid calculate the different..
-    if(date1.getTime() && date2.getTime()){
-        let timeDifferent = date2.getTime()- date1.getTime();
-        let dayDifferent = timeDifferent / (1000 * 3600 * 24);
+        var startDate = $('#start-date').val();
+        var endDate = $('#end-date').val();
 
-        span[0].innerHTML = dayDifferent;
-        span[1].innerHTML = dayDifferent*24;
-        span[2].innerHTML = dayDifferent*24*60;
-        span[3].innerHTML = dayDifferent*24*60*60;
+        // checked i both are date selected 
+        if(!startDate || !endDate) {
+            alert("Please Checked and both date");
+            return;
+        }   
 
-    }
+        // Check if the end date is earlier than the start date
+        var start = new Date(startDate);
+        var end = new Date(endDate);
 
+        // Check if the end date is earlier than the start date
+        if(end < start) {
+            alert("End date cannot be earlier than start date.");
+            return;
+        }
+
+        //calculate the date different to milisecond
+        var differentTime = end - start;
+
+        // convert the time 
+        var milisecondInDay = 1000* 60 * 60 * 24;
+        var milisecondInMonth = milisecondInDay * 30;
+        var millisecondsInYear = milisecondInDay * 365.25;
+
+        var totalDays = Math.floor(differentTime / milisecondInDay);
+        var years = Math.floor(differentTime / millisecondsInYear);
+        var months = Math.floor(differentTime / milisecondInMonth) - years * 12;
+        var days = totalDays - (years * 365) - (months * 30);
+        var hour = totalDays * 24;
+        var min = totalDays * 24 * 60;
+        var sec = totalDays * 24 * 60 * 60;
+
+        console.log(totalDays)
+
+        // Display the result in the respective span elements
+        $("#years").text(years);
+        $("#months").text(months);
+        $("#days").text(days);
+
+        $("#totalDay").text(totalDays);
+        $("#hour").text(hour);
+        $("#min").text(min);
+        $("#sec").text(sec);
+
+    })
 })
+
+
+
+
 
 
 
